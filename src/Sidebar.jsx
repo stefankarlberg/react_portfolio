8be6aec7ad38172dd47ad412c4fb04e6b3d6ed75
stefan_karlberg <stefan.karlberg@smackit.se>
2019-04-16 14:24:15 +0200

    Flexbox style, not yet responsive.

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,15 +1,17 @@
 import React  from "react"
 import { NavLink, Link } from 'react-router-dom';
+import './css/tailwind.css'
+import './css/style.css'
 
 const Sidebar = () => {
         return (
-                <nav className="sidebar">
+                <nav className="p-6">
                 <h2 className="header-title">
-                    <Link className="text-white hover:text-grey no-underline" to='/'>My Portfolio</Link>
+                    <Link className="text-black hover:text-black no-underline  text-4xl" to='/'>LOGO</Link>
                 </h2>
-                <ul className="sidebar_navigation">
-                    <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/about'>About Me</NavLink></li>
-                    <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/projects'>My Projects</NavLink></li>
+                <ul className="sidebar_navigation mt-8 list-reset leading-loose">
+                    <li className="mr-6"><NavLink className="text-black text-xl hover:text-black no-underline" activeStyle={{fontWeight: "bold"}} to='/about'>About Me</NavLink></li>
+                    <li className="mr-6"><NavLink className="text-black text-xl  hover:text-black no-underline" activeStyle={{fontWeight: "bold"}} to='/projects'>My Projects</NavLink></li>
                 </ul>
             </nav>
         )   
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import Hello from "./Hello"
 import Header from "./Header"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
+import './css/tailwind.src.css'
 import './css/tailwind.css'
+
 import { BrowserRouter } from 'react-router-dom'
 import Projects from "./Projects"
 import About from "./About"
@@ -12,27 +14,41 @@ import { Switch, Route } from 'react-router-dom'
 
 const App = () => {
     return (
-        <div className="page-wrapper">
-                <div className="page-content">
-                    <div class="w-1/5 bg-grey h-12">
-                        <Sidebar />
-                    </div>
-                    <div class="w-4/5 bg-grey-light h-12">
-                        <div className="content-wrapper">
-                            <Switch>
-                                <Route exact path='/' component={Hello}></Route>
-                                <Route exact path='/about' component={About}></Route>
-                                <Route exact path='/projects' component={Projects}></Route>
-                            </Switch>
-                            <Hello />
+       <div>
+            <div className="h-screen w-full sm:flex">
+                <div className="landing_left h-screen p-4 pt-8 pb-8">
+                <Sidebar />
+                </div>
+                <div className="landing_right h-screen">
+                    <div className="p-8 h-full">
+                        <div className="hero w-full h-full object-center">
+                            <h1><span className="hero">WHY ME</span></h1>
                         </div>
                     </div>
                 </div>
-        </div >
+            </div >
 
+            <div className="mb-4">
+                <div className="w-full pt-8 text-center">
+                <h2 className="text-black">PORTFOLIO</h2>
+                </div>
+                <div className="mt-6 object-center "> 
+                    <Hello />
+                    <Switch>
+                        <Route exact path='/' component={Hello}></Route>
+                        <Route exact path='/about' component={About}></Route>
+                        <Route exact path='/projects' component={Projects}></Route>
+                    </Switch>
+                </div>    
+            </div>
 
-            
                
+        </div>
+
+
+      
+
+         
            
        
 )
