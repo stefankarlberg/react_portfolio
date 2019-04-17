c0b0ba1e9cf98de73d712d8aa7df792f5ce4d39f
stefan_karlberg <stefan.karlberg@smackit.se>
2019-04-17 11:12:38 +0200

    added divs for clustering information

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,35 @@
 import React from "react"
 
+import './css/tailwind.src.css'
+import './css/tailwind.css'
+import './css/style.css'
+
 const About = () => {
     return (
-        <div className="mt-32" >
-        <h1 className="mb-8">ABOUT ME</h1>
-            <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-        </div>)
+        <div className="page_container" >
+            <div className="intro_container">
+                <p className="intro">Ipsum sdolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias? or dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? or dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? </p>
+            </div>
+            <div class="page_grid">
+                <div class="page_grid_grid_item md:mr-3">
+                    <h2 className="text-black">Headline</h2>
+                </div>
+                <div class="page_grid_grid_item md:ml-3">
+                    <p className="text-black">Psum sdolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga sa. dolor dolorem consectetur est velit fugiat. Dolorem provident co dolor dolorem consectetur est velit fugiat. Dolorem provident co</p>
+                </div>
+            </div>
+            <div class="page_grid">
+                <div class="page_grid_grid_item md:mr-3">
+                    <h2 className="text-black">Headline 2</h2>
+                </div>
+                <div class="page_grid_grid_item md:ml-3">
+                    <p className="text-black">Psum sdolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga sa. dolor dolorem consectetur est velit fugiat. Dolorem provident co dolor dolorem consectetur est velit fugiat. Dolorem provident co</p>
+                </div>
+            </div>
+        </div>
+         
+       
+      )
 }
 
 export default About
