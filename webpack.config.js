7ec353709433a03c6e0d604768403b7347552aec
stefan_karlberg <stefan.karlberg@smackit.se>
2019-04-19 09:14:56 +0200

    Finally undertstands the build/deploy settings in package.json/webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
+    path: path.resolve(__dirname, "build/"),
     publicPath: "/dist/",
     filename: "bundle.js"
   },
